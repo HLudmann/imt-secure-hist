986a0c37125d216a64fb09f36688bb3758c19a5d
Hugo Ludmann <hugo.ludmann@imt-atlantique.net>
2020-03-13 11:30:05 +0100

    added paillier class

---
+++ paillier.py
@@ -0,0 +1,9 @@
+# Implementation of Paillier Cryptosystem
+# with simplification for equivalent length prime number
+import gmpy2
+
+class Paillier(object):
+
+    @staticmethod
+    def gem_prime():
+        
